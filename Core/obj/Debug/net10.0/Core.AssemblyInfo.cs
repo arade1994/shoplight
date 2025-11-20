f34bed174be1d6a1f0a6c95f5254b314801ca6a6
Ante Rade <anterade99@gmail.com>
2025-11-20 21:00:16 +0100

    feat: implemented product entity class

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53166c49a69f14be6d00a672332b4c07e0dc7da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78aabf27a3f363ccdddaa78692da3f82fc4f774")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
